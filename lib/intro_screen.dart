08f57c09324471c689281476ae64f5f6aea524b6
dendenbrn <denrickangelo@gmail.com>
2025-06-02 01:05:34 +0800

    Added intro screen

---
+++ lib/intro_screen.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+
+import 'order_tracking_page.dart';
+
+class IntroScreen extends StatelessWidget {
+  const IntroScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset(
+              'assets/combined_logo.png',
+              height: 150,
+              width: 150,
+            ),
+            const SizedBox(height: 50),
+            
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => OrderTrackingPage()),
+                );
+              },
+              style: ElevatedButton.styleFrom(
+                padding: const EdgeInsets.symmetric(horizontal: 50, vertical: 15),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(30),
+                )
+              ),
+                child: const Text('Sign In', style: TextStyle(fontSize: 16)),
+            ),
+            const SizedBox(height: 20),
+
+            OutlinedButton(
+              onPressed: () {
+
+              },
+              style: OutlinedButton.styleFrom(
+                padding: const EdgeInsets.symmetric(horizontal: 50, vertical: 15),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(30)
+                ),
+                side: const BorderSide(color: Colors.blue),
+              ),
+              child: const Text('Sign Up', style: TextStyle(fontSize: 16))
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
