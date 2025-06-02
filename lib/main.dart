e39a09987260c3ea5cb93bb76966340eee79c4e6
dendenbrn <denrickangelo@gmail.com>
2025-06-02 13:32:44 +0800

    update

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:location/location.dart';
 import 'package:safe_city/chatbot.dart';
+import 'intro_screen.dart';
 
 void main() {
   runApp(MyApp());
@@ -12,7 +13,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: MapPage(),
+      home: IntroScreen(),
     );
   }
 }
